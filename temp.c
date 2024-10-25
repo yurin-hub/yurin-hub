14c24d60a24080898e27c785fac9ff8d9298cf20
yurin-hub <yuring071213@gmail.com>
2024-10-25 22:49:58 +0900

    간단입출력.c
    간단하게 조져봣어요

---
+++ temp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+    int a,b;
+    printf ("덧셈을 진행 할 두 수를 입력하시오: ");
+    scanf ("%d %d",&a,&b);
+    
+    printf ("입력하신 두 수: %d %d",a,b);
+    printf ("더한 값: %d",a+b);
+    
+    
